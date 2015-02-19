760ab4c75ad64ecd9142b3fefa64f0eaa6e1142a
NemoChenTW <nemochentw@gmail.com>
2015-02-19 14:53:50 +0800

    Remove duplicate function in NovelChapterParser.

--- src/NovelChapterParser.cpp
+++ src/NovelChapterParser.cpp
@@ -9,7 +9,6 @@
 #include "Utility/FileUtils.h"
 
 #include <iostream>
-#include <dirent.h>
 
 NovelChapterParser::NovelChapterParser()
 {
@@ -34,52 +33,6 @@ void NovelChapterParser::setDefaultRegExp()
 }
 
 /**
- * @brief	Open in and out file.
- *
- * @param 	novelName	Input file name.
- *
- * @retval	 0	File open success.
- * @retval	-1	File open fail.
- * @retval	-2	File path is empty.
- * @retval	-3	Output file open failed.
- */
-int NovelChapterParser::fileOpen(string novelName)
-{
-	if(!novelName.empty())
-	{
-		string inFilePath = _tempDir + "/" + novelName;
-		string outFilePath = _resultDir + "/" + novelName;
-
-		_fileInStream.open(inFilePath.c_str(), ios::in);
-
-		if (!_fileInStream)
-			return -1;
-		else
-		{
-			cout << novelName << endl;
-			_fileOutStream.open(outFilePath.c_str(), ios::out);
-			if(!_fileOutStream)
-				return -3;
-
-			return 0;
-		}
-	}
-	else
-	{
-		return -2;
-	}
-}
-
-/**
- * @brief	Close in and out files.
- */
-void NovelChapterParser::fileClosed()
-{
-	_fileInStream.close();
-	_fileOutStream.close();
-}
-
-/**
  * @brief	Set regular expression.
  *
  * @param regularExpression		Input regular expression.
@@ -87,20 +40,6 @@ void NovelChapterParser::fileClosed()
 void NovelChapterParser::setRegularExp(string regularExpression)
 {
 	_regexList.push_back(regex(regularExpression));
-}
-
-/**
- * @brief	Check the work directory.
- *
- * Create working directories if they are not exist.
- */
-void NovelChapterParser::checkWorkingDirectory()
-{
-	_tempDir = "temp";
-	_resultDir = "result";
-
-	FileUtils::createDir(_tempDir.c_str());
-	FileUtils::createDir(_resultDir.c_str());
 }
 
 /**
@@ -132,35 +71,6 @@ void NovelChapterParser::parseChapter()
 		fileClosed();
 	}
 
-}
-
-/**
- * @brief	Analysis the temp folder.
- *
- * Analysis the temp folder, and save the file name.
- */
-void NovelChapterParser::analysisFile()
-{
-	struct dirent **namelist;
-	int fileCount = scandir(_tempDir.c_str(), &namelist, 0, alphasort);
-	if(fileCount < 0)
-	{
-		perror("scandir");
-	}
-	else
-	{
-		for(int i = 0; i < fileCount; i++)
-		{
-			string fileName = namelist[i]->d_name;
-			if(fileName == "." || fileName == "..")
-				continue;
-			else if(fileName[fileName.length()-1] == '~')
-				continue;
-			else
-				_inputNovel.push_back(namelist[i]->d_name);
-		}
-	}
-	free(namelist);
 }
 
 /**
--- src/header/NovelChapterParser.h
+++ src/header/NovelChapterParser.h
@@ -12,7 +12,8 @@
 #include <fstream>
 #include <regex>
 #include <list>
-#include <vector>
+
+#include "NovelProcessTool.h"
 
 using namespace std;
 
@@ -25,32 +26,14 @@ public:
 	void parseChapter();
 
 private:
-	fstream 		_fileInStream;		///< The file input stream.
-	fstream 		_fileOutStream;		///< The file out stream.
-
-	string			_tempDir;			///< The temp folder directory path.
-	string			_resultDir;			///< The result folder directory path.
 
 	list<regex> 	_regexList;			///< Regular expression list.
-	vector<string> 	_inputNovel;		///< The input file name.
 
 	///		Set default regular expression.
 	void setDefaultRegExp();
 
 	///		Set regular expression.
 	void setRegularExp(string regularExpression);
-
-	///		Check the work directory.
-	void checkWorkingDirectory();
-
-	///		Open in and out file.
-	int fileOpen(string novelName);
-
-	///		Close in and out files.
-	void fileClosed();
-
-	///		Analysis the temp folder.
-	void analysisFile();
 
 	///		Parse the novel contents.
 	void parseContents();
