95366da812be998c753080005ca35a6ef6797f6d
NemoChenTW <nemochentw@gmail.com>
2015-02-19 14:48:49 +0800

    Create Class NovelProcessTool.

---
+++ src/NovelProcessTool.cpp
@@ -0,0 +1,113 @@
+/*
+ * NovelProcessTool.cpp
+ *
+ *  Created on: 2015年2月19日
+ *      Author: nemo
+ */
+
+#include "header/NovelProcessTool.h"
+#include "Utility/FileUtils.h"
+
+#include <iostream>
+#include <dirent.h>
+
+NovelProcessTool::NovelProcessTool()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+NovelProcessTool::~NovelProcessTool()
+{
+	// TODO Auto-generated destructor stub
+}
+
+/**
+ * @brief	Check the work directory.
+ *
+ * Create working directories if they are not exist.
+ */
+void NovelProcessTool::checkWorkingDirectory()
+{
+	_tempDir = "temp";
+	_resultDir = "result";
+
+	FileUtils::createDir(_tempDir.c_str());
+	FileUtils::createDir(_resultDir.c_str());
+}
+
+/**
+ * @brief	Open in and out file.
+ *
+ * @param 	novelName	Input file name.
+ *
+ * @retval	 0	File open success.
+ * @retval	-1	File open fail.
+ * @retval	-2	File path is empty.
+ * @retval	-3	Output file open failed.
+ */
+int NovelProcessTool::fileOpen(string novelName)
+{
+	if(!novelName.empty())
+	{
+		string inFilePath = _tempDir + "/" + novelName;
+		string outFilePath = _resultDir + "/" + novelName;
+
+		_fileInStream.open(inFilePath.c_str(), ios::in);
+
+		if (!_fileInStream)
+			return -1;
+		else
+		{
+			cout << novelName << endl;
+			_fileOutStream.open(outFilePath.c_str(), ios::out);
+			if(!_fileOutStream)
+				return -3;
+
+			return 0;
+		}
+	}
+	else
+	{
+		return -2;
+	}
+}
+
+/**
+ * @brief	Close in and out files.
+ */
+void NovelProcessTool::fileClosed()
+{
+	_fileInStream.close();
+	_fileOutStream.close();
+}
+
+
+/**
+ * @brief	Analysis the temp folder.
+ *
+ * Analysis the temp folder, and save the file name.
+ */
+void NovelProcessTool::analysisFile()
+{
+	struct dirent **namelist;
+	int fileCount = scandir(_tempDir.c_str(), &namelist, 0, alphasort);
+	if(fileCount < 0)
+	{
+		perror("scandir");
+	}
+	else
+	{
+		for(int i = 0; i < fileCount; i++)
+		{
+			string fileName = namelist[i]->d_name;
+			if(fileName == "." || fileName == "..")
+				continue;
+			else if(fileName[fileName.length()-1] == '~')
+				continue;
+			else
+				_inputNovel.push_back(namelist[i]->d_name);
+		}
+	}
+	free(namelist);
+}
---
+++ src/header/NovelProcessTool.h
@@ -0,0 +1,45 @@
+/*
+ * NovelProcessTool.h
+ *
+ *  Created on: 2015年2月19日
+ *      Author: nemo
+ */
+
+#ifndef SRC_NOVELPROCESSTOOL_H_
+#define SRC_NOVELPROCESSTOOL_H_
+
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class NovelProcessTool {
+public:
+	NovelProcessTool();
+	virtual ~NovelProcessTool();
+
+protected:
+
+	///		Check the work directory.
+	void checkWorkingDirectory();
+
+	///		Open in and out file.
+	int fileOpen(string novelName);
+
+	///		Close in and out files.
+	void fileClosed();
+
+	///		Analysis the temp folder.
+	void analysisFile();
+
+	fstream 		_fileInStream;		///< The file input stream.
+	fstream 		_fileOutStream;		///< The file out stream.
+
+	string			_tempDir;			///< The temp folder directory path.
+	string			_resultDir;			///< The result folder directory path.
+
+	vector<string> 	_inputNovel;		///< The input file name.
+};
+
+#endif /* SRC_NOVELPROCESSTOOL_H_ */
